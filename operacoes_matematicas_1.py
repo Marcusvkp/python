eceefbc11e74fafd447eec8f1cd4e8bf3bc29ee4
Marcus Kuhn <mkparanho@gmail.com>
2022-07-16 23:00:14 -0300

    new commit

---
+++ operacoes_matematicas_1.py
@@ -0,0 +1,15 @@
+operacao_1 = 2 + 3 * 5
+
+operacao_2 = (2 + 3)*5
+
+operacao_3 = 4 / 2 ** 2
+
+operacao_4 = 13 % 3 + 4
+
+print(f"Resultado em operacão_1 = {operacao_1}")
+
+print(f"Resultado em operacão_2 = {operacao_2}")
+
+print(f"Resultado em operacão_4 = {operacao_3}")
+
+print(f"Resultado em operacão_4 = {operacao_4}")
