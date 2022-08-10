617672443339729cf73c46f8e3c7625400a2e75a
Marcus Kuhn <mkparanho@gmail.com>
2022-08-09 23:16:31 -0300

    Calcular fatorial

---
+++ Calculo_fatorial.py
@@ -0,0 +1,9 @@
+def fatorial (numero):
+    if numero == 0 or numero == 1:
+        return 1
+    else:
+        return numero * fatorial (numero -1)
+    
+x = int(input("Digite um numero para o fatorial:"))
+resultado = fatorial(x)
+print("O fatorial de %d é %d" % (x, resultado ))
