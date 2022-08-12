ef44e1daef8672ac97931bbb7f4841e9be9f1ed2
Marcus Kuhn <mkparanho@gmail.com>
2022-08-11 23:30:38 -0300

    Calculadora_IMC e update .gitignore

--- Calculadora_IMC.py
+++ Calculadora_IMC.py
@@ -1,8 +1,14 @@
+# Recebe os valores digitados pelo usuário.
+
 peso = float(input("Digite aqui seu peso (Kg): "))
 altura = float(input("Digite aqui a sua altura (m):"))
 
+# Cálcula e retorna o valor do IMC.
+
 imc = peso / (altura ** 2)
 print("O seu IMC é de {:.1f}".format(imc))
+
+# Compara a condição e retorna a faixa de peso.
 
 if imc < 18.5:
     print("Você está ABAIXO DO PESO normal!")
@@ -14,7 +20,7 @@ elif 25 <= imc < 30:
     print("Você está em SOBREPESO!")
 
 elif 30 <= imc < 40:
-     print("Você está em OBESIDADE!")
+    print("Você está em OBESIDADE!")
 
 else:
-     print("Você está em OBESIDADE MÓRBIDA!")
+    print("Você está em OBESIDADE MÓRBIDA!")
