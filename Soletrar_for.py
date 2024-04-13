0ffcf753fd67523b11d3a081a16e7963dbd305e1
Marcus Kuhn <66495288+Marcusvkp@users.noreply.github.com>
2024-04-12 21:15:53 -0300

    Update Soletrar_for.py

--- Soletrar_for.py
+++ Soletrar_for.py
@@ -1,5 +1,8 @@
-nome = "Power_Guido"
+# Define a string nome
+nome = "Marcus_Kuhn"
 
-for i, c in enumerate (nome):
-    
+# Itera sobre os caracteres da string nome usando a função enumerate
+# A função enumerate retorna um índice (i) e o valor (c) correspondente da lista ou string
+for i, c in enumerate(nome):
+    # Imprime a posição (índice) e o valor (caractere) correspondente
     print(f"Posição = {i}, valor = {c}")
