3f8170c3abaa50209791bf997723949034808218
Marcus Kuhn <66495288+Marcusvkp@users.noreply.github.com>
2024-04-12 21:22:42 -0300

    Update for_enumerate.py

--- for_enumerate.py
+++ for_enumerate.py
@@ -5,10 +5,13 @@ Muito longo para os que lamentam
 Muito curto para os que festejam
 Mas, para os que amam, o tempo é eterno."""
 
+# Iterando sobre cada caractere do texto junto com seu índice
 for i, c in enumerate(texto):
-    if c =='a' or c =='e':
-    
-        print(f"Vogal'{c}' encontrada, na posição {i}")
+    # Verificando se o caractere é 'a' ou 'e'
+    if c == 'a' or c == 'e':
+        # Imprimindo a vogal encontrada e sua posição
+        print(f"Vogal '{c}' encontrada, na posição {i}")
     
     else:
+        # Continuando para a próxima iteração do loop
         continue
