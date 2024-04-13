5dda2f4379ba237b115fd1e66c729ab034c4a2f6
Marcus Kuhn <66495288+Marcusvkp@users.noreply.github.com>
2024-04-12 21:23:59 -0300

    Update nome.py

--- nome.py
+++ nome.py
@@ -1,2 +1,6 @@
+# Solicitando ao usuário que digite seu nome e armazenando-o na variável 'nome'
 nome = input("Digite seu nome:")
+
+# Imprimindo uma mensagem de boas-vindas com o nome inserido pelo usuário
+# Utilizando o método format() para inserir o valor da variável 'nome' na string
 print("Olá {}, bem vindo a disciplina de programação. Parabéns pelo seu primeiro birulei".format(nome))
