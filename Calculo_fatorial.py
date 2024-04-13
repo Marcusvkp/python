b11c63dbfb6c2ae33c619e0e5157ad524dfb6d88
Marcus Kuhn <66495288+Marcusvkp@users.noreply.github.com>
2024-04-12 21:13:11 -0300

    Update Calculo_fatorial.py

--- Calculo_fatorial.py
+++ Calculo_fatorial.py
@@ -1,9 +1,18 @@
-def fatorial (numero):
+def fatorial(numero):
+    # Verifica se o número é 0 ou 1
     if numero == 0 or numero == 1:
+        # Se for 0 ou 1, retorna 1 (pois 0! e 1! são 1)
         return 1
     else:
-        return numero * fatorial (numero -1)
-    
-x = int(input("Digite um numero para o fatorial:"))
+        # Se o número não for 0 ou 1, calcula o fatorial recursivamente
+        # Multiplica o número pelo fatorial do número anterior
+        return numero * fatorial(numero - 1)
+
+# Solicita ao usuário um número para calcular o fatorial
+x = int(input("Digite um número para o fatorial: "))
+
+# Chama a função fatorial com o número fornecido e armazena o resultado
 resultado = fatorial(x)
-print("O fatorial de %d é %d" % (x, resultado ))
+
+# Imprime o número fornecido e seu fatorial calculado
+print("O fatorial de %d é %d" % (x, resultado))
